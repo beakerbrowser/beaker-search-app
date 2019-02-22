e9ea0d75db7d4b9d55d967724ec0c80084591a45
Paul Frazee <pfrazee@gmail.com>
2019-02-22 10:09:29 -0600

    Add missing filters-sidebar files

---
+++ css/com/filters-sidebar.css.js
@@ -0,0 +1,41 @@
+import {css} from '/vendor/beaker-app-stdlib/vendor/lit-element/lit-element.js'
+
+const cssStr = css`
+:host {
+  display: block;
+  color: var(--colot-text--muted);
+  font-size: 14px;
+}
+
+h5 {
+  margin: 0 0 15px;
+}
+
+ul {
+  list-style: none;
+  margin: 0 0 15px;
+  padding: 0;
+}
+
+li {
+  margin: 0 0;
+}
+
+a {
+  cursor: pointer;
+}
+
+a:hover {
+  text-decoration: underline;
+}
+
+.fa-check {
+  visibility: hidden;
+  margin-right: 5px;
+}
+
+li.active .fa-check {
+  visibility: visible;
+}
+`
+export default cssStr
